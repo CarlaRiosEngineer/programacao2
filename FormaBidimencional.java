28791814eda2acdfc183862a85c5cd4e62e048c4
Carla Rios <carlariosndh@gmail.com>
2025-11-12 17:48:14 -0300

    FormaBidimensional

---
+++ FormaBidimencional.java
@@ -0,0 +1,9 @@
+public abstract class FormaBidimencional extends FormaGeometrica {
+
+    public FormaBidimencional(String nome) {
+            super(nome);
+        }
+    public abstract double calcularPerimetro();
+    
+    }
+
